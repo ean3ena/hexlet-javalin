42bbdbfacc939c38706270f0eb02d67c7c4e82f0
ean3ena <ean3ena@yandex.ru>
2024-11-30 22:16:09 +0300

    add new request handler

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,12 +4,19 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
+
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
-        app.start(7090); // Стартуем веб-сервер
+
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result(String.format("Hello, %s!", name));
+        });
+
+        app.start(7090);
     }
 }
