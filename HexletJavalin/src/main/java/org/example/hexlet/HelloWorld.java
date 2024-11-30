02b0bb9472a99260e68beaa597b204426c5bf194
ean3ena <ean3ena@yandex.ru>
2024-12-01 01:22:57 +0300

    add new request handler

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -12,6 +12,12 @@ public class HelloWorld {
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
 
+        app.get("users/{id}/post/{postId}", ctx -> {
+            var userId = ctx.pathParam("id");
+            var postId = ctx.pathParam("postId");
+            ctx.result("User ID: " + userId + " Post ID: " + postId);
+        });
+
         app.get("/hello", ctx -> {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result(String.format("Hello, %s!", name));
